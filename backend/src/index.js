2b420839c056bc80fc1db04a5ea200011cf4e197
Mateus Leonhardt <mateus.leonhardt@conectt.com.br>
2020-01-31 12:39:34 -0300

    Websocket added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0-ge0wa.mongodb.net/devradar?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+setupWebsocket(server);
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
